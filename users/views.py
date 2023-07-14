be3cfc8ce65fd25dec356a4f85344e3859f5a4d0
Axopher <69104459+Axopher@users.noreply.github.com>
2023-07-14 11:46:15 +0545

    Made Mobile Responsive!!!

--- users/views.py
+++ users/views.py
@@ -33,8 +33,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
 
 
-
-
 # Create your views here.
 def home(request):
     plans = Subscription.objects.all()
